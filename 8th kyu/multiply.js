103c48a9e670561edddd2d0ba60ea9c662029e30
Rodney Foster Jr <rodney.fosterbfhs@gmail.com>
2022-06-02 13:50:58 -0400

    New solutions

---
+++ 8th kyu/multiply.js
@@ -0,0 +1,14 @@
+// Kata: This code does not execute properly. Try to figure out why.
+// Original
+/*
+    function multiply(a, b){
+  a * b
+}
+
+*/
+
+// My solution
+function multiply(a, b){
+    return a * b
+   }
+   
