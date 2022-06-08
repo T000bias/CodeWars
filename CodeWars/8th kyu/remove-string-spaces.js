3cf48f3b625727809420b01ee277f1a2173a3caa
Rodney Foster Jr <rodney.fosterbfhs@gmail.com>
2022-06-08 15:42:43 -0400

    8kyu - Opposite number solved

---
+++ CodeWars/8th kyu/remove-string-spaces.js
@@ -0,0 +1,8 @@
+// Kata: Simple, remove the spaces from the string, then return the resultant string.
+// My solution
+function noSpace(x){
+    console.log(x)
+    return x.split(" ").join("");
+    console.log(x)
+    
+  }
