93da78b490903cce08032b212107195129963c3b
[T000bias ] <rodney.fosterbfhs@gmail.com>
2022-08-24 23:40:33 -0400

    new solution:do I get a bonus?

---
+++ JavaScript Solutions/8th kyu/do I get a bonus?.js
@@ -0,0 +1,12 @@
+function bonusTime(salary, bonus) {
+  let multiplier = 10;
+  let convert;
+  let euro = "£";
+  if (bonus === true) {
+    return `£${salary*10}`
+//     console.log(salary);
+  } else {
+    return `£${salary}`
+  }
+// your code here
+}
