62c5d2f8656565f32b0fc29542b1aca651c74d2b
[T000bias ] <rodney.fosterbfhs@gmail.com>
2022-08-25 13:41:20 -0400

    New Solution: name-shuffler

--- JavaScript Solutions/8th kyu/name-shuffler.js
+++ JavaScript Solutions/8th kyu/name-shuffler.js
@@ -5,5 +5,17 @@
 // "john McClane" --> "McClane john"
 
 function nameShuffler(str){
-    //Shuffle It
-  }
+  //Shuffle It
+  let stringArray = str.split(" ");
+  let newArray = []
+  console.log(stringArray);
+  let front = stringArray[1];
+  newArray.push(front);
+  let back = stringArray[0];
+  newArray.push(back);
+//   console.log(front);
+//   console.log(back);
+  console.log(newArray);
+  return newArray.join(' ')
+  
+}
