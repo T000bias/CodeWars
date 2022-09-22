1134823164063d55aff90b5cfbe99bff16aa6045
[T000bias ] <rodney.fosterbfhs@gmail.com>
2022-09-22 16:27:19 -0400

    New Solution: Beginner Series #2 clock

---
+++ JavaScript Solutions/8th kyu/Beginner Series #2 Clock.js
@@ -0,0 +1,8 @@
+// Solution
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  let hours = h * 3600000;
+  let minutes = m * 60000;
+  let seconds = s * 1000;
+  return hours + minutes + seconds
+}
