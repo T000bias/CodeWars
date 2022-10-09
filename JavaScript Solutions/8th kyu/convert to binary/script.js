b587768435fc3f8fc345f94d7ff7f4540d61d547
[T000bias ] <rodney.fosterbfhs@gmail.com>
2022-10-09 13:19:00 -0400

    New Solution: Convert to Binary

---
+++ JavaScript Solutions/8th kyu/convert to binary/script.js
@@ -0,0 +1,21 @@
+// Solution
+function toBinary(n){
+  switch (n) {
+  case 1:
+      return 1
+      break;
+  case 2:
+      return 10
+      break;
+  case 3:
+      return 11
+      break;
+  case 4:
+      return 100
+      break;
+  case 5:
+      return 101
+      break;
+}
+  return n;
+}
