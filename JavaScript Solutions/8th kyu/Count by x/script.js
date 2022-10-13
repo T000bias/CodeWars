ed9c6354dad10d50bcadb230c7baea6c9136de25
T000bias <rodney.fosterbfhs@gmail.com>
2022-10-13 00:31:59 -0400

    New Kata: Count by X

---
+++ JavaScript Solutions/8th kyu/Count by x/script.js
@@ -0,0 +1,10 @@
+// Solutions
+function countBy(x, n) {
+    for (let i = 0; i < n; i++) {
+      let copy = 0;
+      console.log(x)
+      x += 1
+      console.log(copy)
+      copy += x
+    }
+  }
