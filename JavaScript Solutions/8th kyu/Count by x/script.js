886a4200a3fd37e89fb3ea3d770026518f06ce46
T000bias <rodney.fosterbfhs@gmail.com>
2022-10-16 22:31:08 -0400

    New Solution: Count by x

--- JavaScript Solutions/8th kyu/Count by x/script.js
+++ JavaScript Solutions/8th kyu/Count by x/script.js
@@ -1,10 +1,10 @@
 // Solutions
 function countBy(x, n) {
-    for (let i = 0; i < n; i++) {
-      let copy = 0;
-      console.log(x)
-      x += 1
-      console.log(copy)
-      copy += x
-    }
+  let arr = [];
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+//     console.log(total += x)
+    arr.push(total += x)
   }
+  return arr
+}
