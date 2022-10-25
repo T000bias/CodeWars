c8ef7dc98dd94cdf394c9dfffe4f649c7eadb484
T000bias <rodney.fosterbfhs@gmail.com>
2022-10-24 20:58:44 -0400

    Another Solution

--- JavaScript Solutions/8th kyu/will there be enough space?/script.js
+++ JavaScript Solutions/8th kyu/will there be enough space?/script.js
@@ -1,8 +1,8 @@
 // solution
 function enough(cap, on, wait) {
     // your code here
-   let total = 0, overCapcity = 0;
-    total = cap - on
+//    let total = 0, overCapcity = 0;
+    let total = cap - on
     console.log(total)
     if (total - wait >= 0) {
       console.log(cap,on,wait)
@@ -11,3 +11,9 @@ function enough(cap, on, wait) {
       return wait - total
     }
   }
+
+function enough2(cap, on, wait) {
+    // your code here
+    let total = cap - on
+    return total - wait >= 0 ? 0 : wait - total
+}
