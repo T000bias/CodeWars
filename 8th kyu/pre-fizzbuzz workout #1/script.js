ac01d24d3a1b0966970318a75f43130125f23445
T000bias <rodney.fosterbfhs@gmail.com>
2022-11-03 15:49:05 -0400

    New Solutions

--- 8th kyu/pre-fizzbuzz workout #1/script.js
+++ 8th kyu/pre-fizzbuzz workout #1/script.js
@@ -1,12 +1,10 @@
 // Solution
 function preFizz(n) {
     let arr = [];
-    for (let i = 1; i < n.length; i++) {
+    for (let i = 1; i < n; i++) {
       arr.push(i)
-      console.log(arr)
+    //   console.log(arr)
     }
-    return arr
-  }
+return arr
+}
 
-  preFizz(1)
-  
