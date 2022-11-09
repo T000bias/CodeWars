025e5839c17d292f917decc1c04cb92af3447639
T000bias <rodney.fosterbfhs@gmail.com>
2022-11-08 23:00:03 -0500

    New Solution: Make a function that does arithmetic!

---
+++ 7th kyu/Make a function that does arithmetic/script.js
@@ -0,0 +1,15 @@
+// solution
+function arithmetic(a, b, operator){
+    //your code here!
+    switch(operator) {
+        case 'add':
+          return a + b
+        case 'subtract':
+          return a - b
+        case 'multiply':
+          return a * b
+        case 'divide':
+          return a / b
+    }
+  }
+  
