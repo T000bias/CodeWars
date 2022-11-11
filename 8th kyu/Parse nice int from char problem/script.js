85e33db6223ffe970ed8386bb6a5910b22f9029f
T000bias <rodney.fosterbfhs@gmail.com>
2022-11-11 17:29:25 -0500

    New Solution: Parse nice int from char problem

---
+++ 8th kyu/Parse nice int from char problem/script.js
@@ -0,0 +1,9 @@
+// Solution
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :)
+    //   console.log(parseInt(inputString.split('')))
+    //   return `${toNumber(parseInt(inputString.split('')))}`
+      let integer = parseInt(inputString.split(''))
+      console.log(integer)
+      return integer
+    }
