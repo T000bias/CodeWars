d008c43406881f6d1e163088ab7d3ef0eb57aa50
T000bias <rodney.fosterbfhs@gmail.com>
2022-11-20 20:23:16 -0500

    JavaScrip Solution

---
+++ 8th kyu/Area or Perimeter/scipt.js
@@ -0,0 +1,13 @@
+// Solution
+const areaOrPerimeter = function(l , w) {
+    // Return your answer
+    let square = 0
+    let perimeter = 0
+    if (l == w) {
+      return square = l * w
+    }
+    
+    if (l != w) {
+      return perimeter = 2 * (l + w)
+    }
+  };
