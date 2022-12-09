b9a9c2a80bf6205c66e8ca389f7698fd43af90c9
T000bias <rodney.fosterbfhs@gmail.com>
2022-12-09 14:51:11 -0500

    JavaScript Solution

---
+++ 8th kyu/Grasshopper/Grade Book/script.js
@@ -0,0 +1,16 @@
+// Solution
+function getGrade (s1, s2, s3) {
+    // Code here
+    let average = (s1 + s2 + s3) /3
+    if (average >= 90) {
+      return 'A'
+    } else if (average >= 80) {
+      return 'B'
+    } else if (average >= 70) {
+      return 'C'
+    } else if (average >= 60) {
+      return 'D'
+    } else {
+      return 'F'
+    }
+  }
