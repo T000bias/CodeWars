9fdb237427844d2f3614f80f2df6cf0495d2ca84
T000bias <rodney.fosterbfhs@gmail.com>
2022-12-31 23:25:00 -0500

    JavaScript Solution

---
+++ 8th kyu/Unfinished Loop/Bug Fixing #1/README.md
@@ -0,0 +1,5 @@
+# Unfinished Loop - Bug Fixing #1 Solution
+
+## Instructions
+
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
---
+++ 8th kyu/Unfinished Loop/Bug Fixing #1/script.js
@@ -0,0 +1,12 @@
+// Solution
+
+function createArray(number){
+    var newArray = [];
+    
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
