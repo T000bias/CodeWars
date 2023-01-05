261671454137b396d0a5c3e1f68749097f172d19
T000bias <rodney.fosterbfhs@gmail.com>
2023-01-04 20:22:39 -0500

    JavaScript Solution

---
+++ 8th kyu/Alan Partridge/Alan Partridge 2 - Apple Turnover/script.js
@@ -0,0 +1,10 @@
+// Solution
+
+function apple(x){
+    let convert = Number(x)
+    if (convert**2 > 1000) {
+      return "It\'s hotter than the sun!!"
+    } else {
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    }
+  }
