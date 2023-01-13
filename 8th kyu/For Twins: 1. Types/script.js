35e905b204e76c157fa5942ee723532d1bc5627e
T000bias <rodney.fosterbfhs@gmail.com>
2023-01-13 17:06:22 -0500

    JavaScript Solution

---
+++ 8th kyu/For Twins: 1. Types/script.js
@@ -0,0 +1,9 @@
+// Solution
+function typeValidation(variable, type) {
+    // Your code should be here ;) 
+    if (typeof variable === type) {
+      return true
+    } else {
+      return false
+    }
+  }
