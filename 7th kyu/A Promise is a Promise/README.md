63d9b2045f960b675af958351052947ea5a8b8d1
T000bias <rodney.fosterbfhs@gmail.com>
2023-01-28 20:02:31 -0500

    JavaScript Solution

---
+++ 7th kyu/A Promise is a Promise/README.md
@@ -0,0 +1,5 @@
+# A Promise is a Promise Solution
+
+## Instructions
+
+Create a resolved javascript Promise that will return 'Hello World!'.
---
+++ 7th kyu/A Promise is a Promise/script.js
@@ -0,0 +1,7 @@
+// Solution
+function promiseHelloWorld() {
+    // replace this nonsense with your Promise
+    return new Promise(function(resolve,reject) {
+      resolve('Hello World!')
+    })
+  }
