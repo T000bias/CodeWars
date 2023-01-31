29767ad65c62781a5cbcd75466a8f24a6c952ba0
T000bias <rodney.fosterbfhs@gmail.com>
2023-01-31 01:55:22 -0500

    JavaScript Solution

---
+++ 8th kyu/101 Dalmations - squash the bugs,  not the dogs!/script.js
@@ -0,0 +1,4 @@
+// Solution
+function howManyDalmatians(number) {
+    return number <= 10 ? 'Hardly any' : number <= 50 ? 'More than a handful!' : number == 101 ? "101 DALMATIANS!!!" : 'Woah that\'s a lot of dogs!'
+  } 
