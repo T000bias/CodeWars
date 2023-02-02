32b0c2e6de42e098325dc60c293af82c0a05b8b4
T000bias <rodney.fosterbfhs@gmail.com>
2023-02-02 01:34:04 -0500

    JavaScript Solution

---
+++ 7th kyu/esreveR/README.md
@@ -0,0 +1,7 @@
+# esreveR Solution
+
+## Instructions
+
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
---
+++ 7th kyu/esreveR/script.js
@@ -0,0 +1,9 @@
+// Solution
+let reverse = function(array) {
+    // TODO: program me!
+    let reversed = []
+    for (let i = array.length - 1; i >= 0; i--) {
+      reversed.push(array[i])
+    }
+    return reversed
+  }
