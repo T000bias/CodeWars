baf829dcbcd9fcd243d9eaf47229197fd9ec03da
T000bias <rodney.fosterbfhs@gmail.com>
2023-02-12 19:02:42 -0500

    Added JavaScript Solution

--- 8th kyu/validate code with simple regex/script.js
+++ 8th kyu/validate code with simple regex/script.js
@@ -1,2 +1,11 @@
 // Solution
 
+function validateCode (code) {
+    //your code here
+      let spread = String(code).split('')
+      if (spread[0] === '1' || spread[0] === '2' || spread[0] === '3') {
+        return true
+      } else {
+        return false
+      }
+    }
