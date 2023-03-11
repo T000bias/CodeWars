65aff68b6b8e876ae56f7d22be9205bae871be14
T000bias <rodney.fosterbfhs@gmail.com>
2023-03-11 07:06:42 -0500

    Added JS solution & README

---
+++ 8th kyu/Grasshopper/Terminal Game #1/script.js
@@ -0,0 +1,9 @@
+// Solution
+function Hero (name = "Hero") {
+    // add default values here
+    this.name = name;
+    this.health = 100
+    this.experience = 0
+    this.position = '00'
+    this.damage = 5
+  }
