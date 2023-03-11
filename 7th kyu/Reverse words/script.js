aa197a7b315166096332b3e24bdeef45675036f0
T000bias <rodney.fosterbfhs@gmail.com>
2023-03-11 07:16:52 -0500

    Added JS Solution & README

---
+++ 7th kyu/Reverse words/script.js
@@ -0,0 +1,10 @@
+// Solution
+function reverseWords(str) {
+    // Go for it
+    let arr = []
+    let string = str.split(' ')
+    for (let word of string) {
+      arr.push(word.split('').reverse().join(''))
+    }
+    return arr.join(' ')
+  }
