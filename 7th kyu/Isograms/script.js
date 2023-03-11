d3a3da49117a3719f9deb35dc5ce17f0b82c2649
T000bias <rodney.fosterbfhs@gmail.com>
2023-03-11 08:08:10 -0500

    Added JS soluton & README

---
+++ 7th kyu/Isograms/script.js
@@ -0,0 +1,9 @@
+// Solution
+function isIsogram(str){
+    //...
+    let words = str.toLowerCase().split('')
+    return (words.filter(function(item,index) {
+      return words.indexOf(item) === index
+    }).join('') === str.toLowerCase())
+  
+  }
