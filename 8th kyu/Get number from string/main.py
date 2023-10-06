fb348ed961f5d4f2d3aed4861140acd68006d5d6
T000bias <rodney.fosterbfhs@gmail.com>
2023-10-06 00:01:54 -0400

    new solution

--- 8th kyu/Get number from string/main.py
+++ 8th kyu/Get number from string/main.py
@@ -6,4 +6,8 @@ def get_number_from_string(string):
             sum += value
     return int(sum)
             
-    
+import re
+def get_number_from_string(string):
+    num = re.compile(r'\d')
+    match = num.findall(string)
+    return int(("".join(match)))
